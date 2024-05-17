a534f04abfd2543b609ea49f6fa7f69f50b69c56
nilesh-30 <nileshk7017@gmail.com>
2024-05-17 09:24:55 +0530

    Solved data not showing error and added git ignore

--- index.js
+++ index.js
@@ -16,13 +16,11 @@ app.get('/', function(req,res) {
     });
 });
 
-app.get('/file/:filename', function(req,res){
-    fs.readFile(`./file/${req.params.filename}`, "utf-8", function(err, filedata) {
+app.get('/files/:filename', function(req,res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata) {
         res.render("show", {filename: req.params.filename, filedata: filedata});
     });
 });
-
-
 
 app.get('/edit/:filename', function(req,res){
     res.render('edit', {filename: req.params.filename});
